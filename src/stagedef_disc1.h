99dec1722b0aaf47d719bde8f00fc3b184e6fb0a
UNSTOP4BLE <bruno.juraski1@gmail.com>
2022-06-06 19:18:37 +0200

    Initial commit

---
+++ src/stagedef_disc1.h
@@ -0,0 +1,17 @@
+	{ //StageId_1_1 (Bopeebo)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+        {NULL},
+		{Char_GF_New,     FIXED_DEC(0,1),  FIXED_DEC(-10,1)},
+		
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(15,10),FIXED_DEC(15,10),FIXED_DEC(15,10)},
+		1, 1,	
+		XA_Bopeebo, 0,
+		
+		StageId_1_1, 0
+	},
