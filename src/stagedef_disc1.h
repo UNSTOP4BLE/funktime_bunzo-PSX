1003837807b11123cc6a9456b5d43c9309757fd7
UNSTOP4BLE <bruno.juraski1@gmail.com>
2022-06-08 23:13:46 +0200

    some fixis

--- src/stagedef_disc1.h
+++ src/stagedef_disc1.h
@@ -1,6 +1,6 @@
 	{ //StageId_1_1 (Bopeebo)
 		//Characters
-		{Char_BF_New,    FIXED_DEC(-60,1),  FIXED_DEC(100,1)},
+		{Char_BF_New,    FIXED_DEC(-55,1),  FIXED_DEC(120,1)},
 		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(0,1)},
         {NULL},
 		{NULL},
